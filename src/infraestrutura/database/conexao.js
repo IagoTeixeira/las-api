5fa8ca3e7092d19da3e142dca7e729652662b89f
Iago Teixeira <iago.teixeira@outlook.com.br>
2022-06-09 13:12:52 -0300

    Subindo db

--- src/infraestrutura/database/conexao.js
+++ src/infraestrutura/database/conexao.js
@@ -6,7 +6,7 @@ const DBUSER = process.env.DBUSER;
 const DBPASS = process.env.DBPASS;
 const DBNAME = process.env.DBNAME;
 
-const pool = mysql.createPool({
+const localConfig = mysql.createPool({
   connectionLimit: 100,
   host: DBHOST || "localhost",
   port: DBPORT || 3306,
@@ -15,4 +15,5 @@ const pool = mysql.createPool({
   database: DBNAME || "las",
 });
 
+const pool = mysql.createPool(process.env.DATABASE_URL || localConfig);
 module.exports = pool;
